1b3a675605982e099c4486493d97028d9103e704
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-04-19 18:56:32 +0600

    Update sw.js

--- audit/sw.js
+++ audit/sw.js
@@ -1,2 +1,5 @@
-/* 🔒 PROTECTED BY VAULT-GUARD v1.0 | LEAD ARCHITECT: MD IBRAHIM HOSSAIN | PIXEL PERFECT IT SOLUTIONS */
-const CACHE_NAME="ppit-hq-v1.1",assets=["/","/index.html"];self.addEventListener("install",(e=>{e.waitUntil(caches.open(CACHE_NAME).then((e=>e.addAll(assets))))})),self.addEventListener("fetch",(e=>{e.respondWith(caches.match(e.request).then((s=>s||fetch(e.request))))}));
+/* 🔒 PROTECTED BY VAULT-GUARD v1.0 | LEAD ARCHITECT: MD IBRAHIM HOSSAIN */
+const CACHE_NAME = 'ppit-hq-v1.1';
+const assets = ['/', '/index.html'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))); });
