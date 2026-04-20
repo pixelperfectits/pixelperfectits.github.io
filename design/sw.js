5526833f2a4157067f1054d863b5d1fbe650da26
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-04-20 21:31:36 +0600

    Delete design directory

--- design/sw.js
+++
@@ -1,5 +0,0 @@
-/* 🔒 PROTECTED BY VAULT-GUARD v1.0 | LEAD ARCHITECT: MD IBRAHIM HOSSAIN */
-const CACHE_NAME = 'design-hub-v1.0';
-const assets = ['/design/', '/design/index.html'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets))); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))); });
