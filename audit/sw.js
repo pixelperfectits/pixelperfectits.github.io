2bd8d3836b6730dbf1a442038a1afa05ded10487
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-04-21 18:53:15 +0600

    Update sw.js

--- audit/sw.js
+++ audit/sw.js
@@ -1,9 +1,9 @@
 /* 🔒 PROPRIETARY OFFLINE ENGINE | LEAD ARCHITECT: MD IBRAHIM HOSSAIN */
-const CACHE_NAME = 'audit-vault-v10';
+const CACHE_NAME = 'audit-vault-v11';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
+  '/audit/',
+  '/audit/index.html',
+  '/audit/manifest.json',
   'https://raw.githubusercontent.com/pixelperfectits/assets/main/Flat%20Minimal%20Neon%20Icon.png',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/lucide@latest'
