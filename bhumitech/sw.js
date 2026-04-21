64c9b9781d14ba0859a742cf9760be3255102d01
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-04-21 11:46:54 +0600

    Update sw.js

--- bhumitech/sw.js
+++ bhumitech/sw.js
@@ -1,5 +1,5 @@
 /* 🔒 PROPRIETARY OFFLINE ENGINE | LEAD ARCHITECT: MD IBRAHIM HOSSAIN */
-const CACHE_NAME = 'bhumitech-vault-v13';
+const CACHE_NAME = 'bhumitech-vault-v14';
 const ASSETS = [
   '/bhumitech/',
   '/bhumitech/index.html',
