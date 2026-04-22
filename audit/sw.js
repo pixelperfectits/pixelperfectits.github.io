0df44170804e0f70b881b39d1bc88c061302e6a8
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-04-22 18:36:29 +0600

    Update sw.js

--- audit/sw.js
+++ audit/sw.js
@@ -8,12 +8,6 @@ const ASSETS = [
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/lucide@latest'
 ];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(i => i !== CACHE_NAME).map(i => caches.delete(i)))));
-});
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(i => i !== CACHE_NAME).map(i => caches.delete(i))))); });
