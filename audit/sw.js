74a87d01fec34821e7919b808bac7f0990fc13aa
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-04-22 21:10:25 +0600

    Update sw.js

--- audit/sw.js
+++ audit/sw.js
@@ -1,5 +1,5 @@
-/* 🔒 PROPRIETARY OFFLINE ENGINE | LEAD ARCHITECT: MD IBRAHIM HOSSAIN */
-const CACHE_NAME = 'audit-vault-v16';
+/* 🔒 PROPRIETARY OFFLINE ENGINE | LEAD ARCHITECT: MD Ibrahim Hossain */
+const CACHE_NAME = 'audit-vault-v17';
 const ASSETS = [
   './',
   './index.html',
