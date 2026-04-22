f0b65db1e74e13512ab651d99b7e5e6954436807
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-04-22 21:36:29 +0600

    Update sw.js

--- visual/sw.js
+++ visual/sw.js
@@ -1,5 +1,5 @@
 /* 🔒 PROPRIETARY OFFLINE ENGINE | LEAD ARCHITECT: MD IBRAHIM HOSSAIN */
-const CACHE_NAME = 'visual-vault-v9';
+const CACHE_NAME = 'visual-vault-v10';
 const ASSETS = [
   './',
   './index.html',
