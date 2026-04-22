dffdc8ed7f21bfebc8777909e5ba1d8123d958ba
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-04-22 13:48:29 +0600

    Update sw.js

--- systems/sw.js
+++ systems/sw.js
@@ -1,5 +1,5 @@
 /* 🔒 PROPRIETARY OFFLINE ENGINE | LEAD ARCHITECT: MD IBRAHIM HOSSAIN */
-const CACHE_NAME = 'systems-vault-v13';
+const CACHE_NAME = 'systems-vault-v14';
 const ASSETS = [
   '/systems/',
   '/systems/index.html',
