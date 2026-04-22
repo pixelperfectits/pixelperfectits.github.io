6a1386b64a7737cde7aec8cc4100dde17d642ab0
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-04-22 18:37:38 +0600

    Update sw.js

--- bhumitech/sw.js
+++ bhumitech/sw.js
@@ -1,19 +1,13 @@
 /* 🔒 PROPRIETARY OFFLINE ENGINE | LEAD ARCHITECT: MD IBRAHIM HOSSAIN */
-const CACHE_NAME = 'bhumitech-vault-v19';
+const CACHE_NAME = 'bhumitech-vault-v20';
 const ASSETS = [
-  '/bhumitech/',
-  '/bhumitech/index.html',
-  '/bhumitech/manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
   'https://raw.githubusercontent.com/pixelperfectits/assets/main/Flat%20Minimal%20Neon%20Icon.png',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/lucide@latest'
 ];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(k => Promise.all(k.filter(i => i !== CACHE_NAME).map(i => caches.delete(i)))));
-});
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(i => i !== CACHE_NAME).map(i => caches.delete(i))))); });
