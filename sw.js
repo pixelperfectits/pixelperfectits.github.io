ccdddedbbb6bb831ad4a7e617a820440f96bb7a9
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-04-22 21:07:03 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 🔒 PROPRIETARY OFFLINE ENGINE | LEAD ARCHITECT: MD IBRAHIM HOSSAIN */
-const CACHE_NAME = 'ppit-hq-vault-v14';
+const CACHE_NAME = 'ppit-hq-vault-v15';
 const ASSETS = [
   './',
   './index.html',
@@ -8,12 +8,6 @@ const ASSETS = [
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/lucide@latest'
 ];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(i => i !== CACHE_NAME).map(i => caches.delete(i)))));
-});
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(i => i !== CACHE_NAME).map(i => caches.delete(i))))); });
