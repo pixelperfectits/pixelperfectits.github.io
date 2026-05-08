7ee7099c91570624452a847c6ac37684fc3eaa9b
MD IBRAHIM HOSSAIN <pixelperfect.its@gmail.com>
2026-05-08 22:05:39 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 🔒 PROPRIETARY OFFLINE ENGINE | LEAD ARCHITECT: MD IBRAHIM HOSSAIN */
-const CACHE_NAME = 'ppit-hq-vault-v15';
+const CACHE_NAME = 'bhumitech-hq-vault-v2';
 const ASSETS = [
   './',
   './index.html',
